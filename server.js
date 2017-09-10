7822dcf19a767eeafc33be4f2026c92ba3f755c8
Joe <joepgribbin@gmail.com>
2017-09-10 14:12:51 -0700

    Set start script and heroku port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');  //Sets up partials, takes argument directory to use
@@ -70,5 +71,5 @@ app.get('/bad', (req, res) => {
 })
 
 app.listen(3000, () => {  //List can can take second optional argument
-  console.log('Server is up on port 3000');
+  console.log(`Server is up on port ${port}`);
 });
